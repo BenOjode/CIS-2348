b69d64902c01ab6fc294c3222e8e220e630f915b
Ben Ojode <116505451+BenOjode@users.noreply.github.com>
2023-06-18 02:18:58 -0500

    ZyLab 1.20

---
+++ Homework1/main(2).py
@@ -0,0 +1,17 @@
+# ZyLab 1.20
+# Benjamin Ojode
+# 1663352
+
+user_num = int(input('Enter integer:\n'))
+
+num_squared = user_num ** 2
+num_cubed = user_num ** 3
+
+print('You entered:', user_num)
+print(user_num, 'squared is', num_squared)
+print('And', user_num, 'cubed is', num_cubed, '!!')
+
+user_num1 = int(input('Enter another integer:\n'))
+
+print(user_num, '+', user_num1, 'is', user_num + user_num1)
+print(user_num, '*', user_num1, 'is', user_num * user_num1)
