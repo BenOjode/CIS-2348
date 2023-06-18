ea0cec8b4f9f04db718f571497c419568c3b549a
BenOjode <116505451+BenOjode@users.noreply.github.com>
2023-06-18 00:44:09 -0500

    ZyLab 1.12 Commit 2

--- Homework1/main.py
+++ Homework1/main.py
@@ -1,7 +1,8 @@
+# ZyLab 1.12
 # Benjamin Ojode
 # 1663352
 
 userNum = int(input())
 userNumSquared = userNum + userNum  # Bug here; fix it when instructed
 
-print(userNumSquared, end=' ')  # Output formatting issue here; fix it when instructed
+print(userNumSquared)  # Output formatting issue here; fix it when instructed
