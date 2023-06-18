92132a965abe5d8a84e835ab2e561682624aeee2
BenOjode <116505451+BenOjode@users.noreply.github.com>
2023-06-18 01:54:14 -0500

    ZyLab 1.20 Commit 1

---
+++ Zylab_1.20/main.py
@@ -0,0 +1,12 @@
+# ZyLab 1.20
+# Benjamin Ojode
+# 1663352
+
+user_num = int(input())
+user_num1 = int(input())
+
+num_squared = user_num ** 2
+
+print('You entered: ',user_num)
+print(user_num, 'squared is ', num_squared)
+print('And', user_num, 'cubed is', )
