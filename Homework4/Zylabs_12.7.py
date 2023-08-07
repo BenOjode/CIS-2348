0ad6f3904f149c46489a2d00a0f9513695d16059
Ben Ojode <116505451+BenOjode@users.noreply.github.com>
2023-08-06 22:48:04 -0500

    Calculates an adult's fat-burning heart rate.

---
+++ Homework4/Zylabs_12.7.py
@@ -0,0 +1,21 @@
+# ZyLabs 12.7
+# Benjamin Ojode
+# 1663352
+
+def get_age():
+    age = int(input())
+    if 18 <= age <= 75:
+        return age
+    raise ValueError("Invalid age.")
+
+def fat_burning_heart_rate(age):
+    heart_rate = (220 - age) * 0.7
+    return heart_rate
+
+if __name__ == "__main__":
+    try:
+        age = get_age()
+        print("Fat burning heart rate for a", age, "year-old:", fat_burning_heart_rate(age), "bpm")
+    except ValueError as i:
+        print(i)
+        print("Could not calculate heart rate info.\n")
